ea41aff4e184513c8e5976b117702f524402a397
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2022-11-22 23:16:28 +0530

    Created test case for book creation

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
-describe "Books Api", type: :request do 
-    it "return all books" do
-        FactoryBot.create(:book, author: "1984", title: "George Orwell")
-        FactoryBot.create(:book, author: "Alchemist", title: "Paulo Cohelo")
+describe "Books API", type: :request do 
+    
+    describe "GET /books" do
+        it "return all books" do
+            FactoryBot.create(:book, author: "1984", title: "George Orwell")
+            FactoryBot.create(:book, author: "Alchemist", title: "Paulo Cohelo")
 
-        get '/api/v1/books'
+            get '/api/v1/books'
 
-        expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body).size).to eq(2)
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(2)
+        end
     end
 
+    describe "POST /books" do
+        it "creates a book" do
+            expect{
+                post '/api/v1/books', params: { book: {author: "Jonathan Swift", title: "Gullivers Travels"}}
+            }.to change { Book.count }.from(0).to(1) 
+
+            expect(response).to have_http_status(:created)
+        end
+    end
 end
